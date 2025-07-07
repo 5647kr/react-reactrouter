1848e1f92c77cf7ff09c7408b9735852ac0910c2
K's_FE <5647kr@gmail.com>
2025-07-07 16:08:43 +0900

    상세 페이지 추가 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Chat from "./pages/Chat/Chat";
 import Profile from "./pages/Profile/Profile"
+import Detail from "./pages/Product/detail/Detail";
 
 
 function App() {
@@ -11,7 +12,10 @@ function App() {
       <BrowserRouter basename="/reactrouter">
         <Routes>
           <Route path="/" element={<Home />}>Home</Route>
-          <Route path="/product" element={<Product />}>Home</Route>
+          <Route path="/product" element={<Product />}> 
+            <Route path="" element={<Outlet />} />
+            <Route path="123" element={<Detail />} />
+          </Route>
           <Route path="/chat" element={<Chat />}>Chat</Route>
           <Route path="/profile" element={<Profile />}>Profile</Route>
         </Routes>
